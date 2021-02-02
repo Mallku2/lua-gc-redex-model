f8efe14bf362ab4c75c31534f724d995aa00cb8a
beta <beta@mpi-sws.org>
2021-02-02 10:42:07 -0300

    non-modularized, but passing, closure test, with added tests

--- Tests/LuaTests/closure.rkt
+++ Tests/LuaTests/closure.rkt
@@ -1,39 +1,18 @@
 #lang racket
 (require redex
-         "../../grammar.rkt"
          "../../executionEnvironment.rkt"
          "../../Relations/fullProgs.rkt"
          "../../Desugar/parser.rkt"
          "./tests_aux.rkt")
 
-(define (lua-closure-test-suite)
-  (check-redundancy #t)
-  (caching-enabled? #t)
-  (test-predicate ok? (apply-reduction-relation*
-                       full-progs-rel
-                       (plugIntoExecutionEnvironment
-                        services
-                        ; TODO: let's make the parser fill this list
-                        (list "assert"
-                              "collectgarbage"
-                              "error"
-                              "pcall"
-                              "print"
-                              "math"
-                              "math.sin")
-                        (parse-this (file->string "closure.lua") #f (void)))))
-  (test-results))
-
-
-(define (lua-closure-partial-test n)
-  (partial_test n "closure"
-                (plugIntoExecutionEnvironment
-                 services
-                 ; TODO: let's make the parser fill this list
-                 (list "assert"
-                       "error"
-                       "pcall"
-                       "print"
-                       "math"
-                       "math.sin")
-                 (parse-this (file->string "closure.lua") #f (void)))))
+(define (closure)
+  (test-suite "closure.lua"
+              (list "assert"
+                    "collectgarbage"
+                    "error"
+                    "pcall"
+                    "print"
+                    "math"
+                    "math.sin"
+                    "setmetatable"
+                    "getmetatable")))
