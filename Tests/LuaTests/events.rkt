a2cac73c87682c059410428652257431a9c4b1db
msoldevila <mallkuernesto@gmail.com>
2021-02-12 13:30:29 -0300

    added license to events.lua; events_3 sliced; events.lua ok

--- Tests/LuaTests/events.rkt
+++ Tests/LuaTests/events.rkt
@@ -1,94 +1,76 @@
 #lang racket
-(require redex
-         "../../executionEnvironment.rkt"
-         "../../Relations/fullProgs.rkt"
-         "../../Desugar/parser.rkt"
-         "./tests_aux.rkt")
+(require "./tests_aux.rkt")
 
+; semantics of set and get metatable
+; metamethod for tostring
+; protected metatables
+; metamethod for new index
 (define (test-events_1)
   (test-suite "events_1.lua"
-              (list "_G"
-                    "assert"
+              (list "assert"
                     "collectgarbage"
                     "getmetatable"
-                    "next"
-                    "pairs"
                     "pcall"
                     "print"
-                    "rawequal"
-                    "rawget"
-                    "rawlen"
                     "rawset"
                     "setmetatable"
-                    "table"
-                    "tostring"
-                    "table.unpack"
-                    "type"
-                    )))
+                    "tostring")))
 
+; metamethod for index and new index
+; metamethods for arith ops
 (define (test-events_2)
   (test-suite "events_2.lua"
-              (list "_G"
-                    "assert"
+              (list "assert"
                     "collectgarbage"
                     "getmetatable"
-                    "next"
-                    "pairs"
-                    "pcall"
-                    "print"
-                    "rawequal"
                     "rawget"
-                    "rawlen"
                     "rawset"
                     "setmetatable"
                     "table"
-                    "tostring"
-                    "table.unpack"
-                    "type"
-                    )))
+                    "table.unpack")))
 
-(define (test-events_3)
-  (test-suite "events_3.lua"
-              (list "_G"
-                    "assert"
+; metamethod for #, <, <=, ==, ..
+(define (test-events_3a)
+  (test-suite "events_3a.lua"
+              (list "assert"
+                    "collectgarbage"
+                    "pcall"
+                    "rawlen"
+                    "setmetatable"
+                    "type")))
+
+(define (test-events_3b)
+  (test-suite "events_3b.lua"
+              (list "assert"
+                    "collectgarbage"
+                    "setmetatable"
+                    "type")))
+
+(define (test-events_3c)
+  (test-suite "events_3c.lua"
+              (list "assert"
                     "collectgarbage"
                     "getmetatable"
                     "next"
                     "pairs"
-                    "pcall"
-                    "print"
                     "rawequal"
-                    "rawget"
-                    "rawlen"
-                    "rawset"
                     "setmetatable"
-                    "table"
-                    "tostring"
-                    "table.unpack"
-                    "type"
-                    )))
+                    "type")))
 
 (define (test-events_4)
   (test-suite "events_4.lua"
               (list "_G"
                     "assert"
-                    "collectgarbage"
-                    "getmetatable"
-                    "next"
-                    "pairs"
-                    "pcall"
-                    "print"
-                    "rawequal"
-                    "rawget"
-                    "rawlen"
-                    "rawset"
-                    "setmetatable"
-                    "table"
-                    "tostring"
-                    "table.unpack"
-                    "type"
                     "debug"
                     "debug.setmetatable"
-                    )))
+                    "getmetatable"
+                    "pcall"
+                    "print"
+                    "rawget"
+                    "rawset"
+                    "setmetatable"
+                    "type")))
 
-(provide test-events_1 test-events_2 test-events_3 test-events_4)
+(provide test-events_1 test-events_2
+         test-events_3a test-events_3b test-events_3c
+         test-events_4)
--- Tests/LuaTests/events_3c.lua
+++ Tests/LuaTests/events_3c.lua
@@ -1,51 +1,27 @@
--- test for rawlen
-t = setmetatable({1,2,3}, {__len = function () return 10 end})
-assert(#t == 10 and rawlen(t) == 3)
-assert(rawlen"abc" == 3)
-------------------------------------------
--- TODO: io.stdin
-------------------------------------------
--- assert(not pcall(rawlen, io.stdin))
-assert(not pcall(rawlen, 34))
-assert(not pcall(rawlen))
+-- Copyright © 1994–2019 Lua.org, PUC-Rio.
 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy 
+-- of this software and associated documentation files (the "Software"), to deal 
+-- in the Software without restriction, including without limitation the rights 
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+-- copies of the Software, and to permit persons to whom the Software is 
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in 
+-- all copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+-- SOFTWARE. 
+
+---------------------------------
+--- taken from events_3b.lua
+--------------------------------
 t = {}
-t.__lt = function (a,b,c)
-  collectgarbage()
-  assert(c == nil)
-  if type(a) == 'table' then a = a.x end
-  if type(b) == 'table' then b = b.x end
- return a<b, "dummy"
-end
-
-function Op(x) return setmetatable({x=x}, t) end
-
-local function test ()
-  assert(not(Op(1)<Op(1)) and (Op(1)<Op(2)) and not(Op(2)<Op(1)))
-  assert(not(1 < Op(1)) and (Op(1) < 2) and not(2 < Op(1)))
-  assert(not(Op('a')<Op('a')) and (Op('a')<Op('b')) and not(Op('b')<Op('a')))
-  assert(not('a' < Op('a')) and (Op('a') < 'b') and not(Op('b') < Op('a')))
-  assert((Op(1)<=Op(1)) and (Op(1)<=Op(2)) and not(Op(2)<=Op(1)))
-  assert((Op('a')<=Op('a')) and (Op('a')<=Op('b')) and not(Op('b')<=Op('a')))
-  assert(not(Op(1)>Op(1)) and not(Op(1)>Op(2)) and (Op(2)>Op(1)))
-  assert(not(Op('a')>Op('a')) and not(Op('a')>Op('b')) and (Op('b')>Op('a')))
-  assert((Op(1)>=Op(1)) and not(Op(1)>=Op(2)) and (Op(2)>=Op(1)))
-  assert((1 >= Op(1)) and not(1 >= Op(2)) and (Op(2) >= 1))
-  assert((Op('a')>=Op('a')) and not(Op('a')>=Op('b')) and (Op('b')>=Op('a')))
-  assert(('a' >= Op('a')) and not(Op('a') >= 'b') and (Op('b') >= Op('a')))
-end
-
-test()
-
-t.__le = function (a,b,c)
-  assert(c == nil)
-  if type(a) == 'table' then a = a.x end
-  if type(b) == 'table' then b = b.x end
- return a<=b, "dummy"
-end
-
-test()  -- retest comparisons, now using both `lt' and `le'
-
 
 -- test `partial order'
 
